d13657d25ba416fdfc65ba30e1aafaa0f07ed603
Wladi <wladik21@hotmail.de>
2024-06-24 09:44:31 +0200

    updated LAHC

--- run.py
+++ run.py
@@ -9,19 +9,19 @@ import lahc
 import os
 import json
 import logging
+import copy
 
 from envs.dynamic_crossing import DynamicCrossingEnv
 from minigrid.core.world_object import Door, Goal, Lava, Wall, Ball, Key
 
-logging.basicConfig(
-    filename='activation_function_optimizer_gelu100k.log', level=logging.DEBUG)
-
 
 class ActivationFunctionOptimizer(lahc.LateAcceptanceHillClimber):
-    def __init__(self, initial_state, agent, env, candidate_activations):
+    def __init__(self, initial_state, agent, env, candidate_activations, memory, args):
         super(ActivationFunctionOptimizer, self).__init__(
             initial_state=initial_state)
         self.agent = agent
+        self.memory = memory
+        self.args = args
         self.env = env
         self.candidate_activations = candidate_activations
 
@@ -34,33 +34,86 @@ class ActivationFunctionOptimizer(lahc.LateAcceptanceHillClimber):
 
     def energy(self):
         total_reward = 0
-        episode_range = 10
-        # @Aditya ----------
-        # I used to loop episode_range times over this part. This is what I thought you meant with call LAHC 10 times
-        # Currently the Search terminates after idle_steps*fraction < steps AND min_steps < steps. Meaning no matter what the loop runs for at least 100k steps,
-        # however 100 step takes about 30 seconds which leads to 8+ hours for 100k steps.
-        # After looking at the states, it seems pointless to continue searching after 5k idle steps, much less 90k like it is now
-        hidden_p = self.agent.get_initial_hidden()
-        action = -1
-        reward = 0
-        state = self.env.reset()[0]["image"].astype(np.float32).reshape(-1)
+        zeroshot = self.args["zero_shot"]
+        episode_range = 5
+        total_numsteps = 50000
 
-        while True:
-            action, hidden_p = self.agent.select_action(
-                state, action, reward, hidden_p, EPS_up=False, evaluate=True
-            )
-            next_state, reward, terminated, truncated, _ = self.env.step(
-                action)
+        if zeroshot:
+            for _ in range(episode_range):
+                hidden_p = self.agent.get_initial_hidden()
+                action = -1
+                reward = 0
+                state = self.env.reset()[0]["image"].astype(
+                    np.float32).reshape(-1)
 
-            total_reward += reward
+                while True:
+                    action, hidden_p = self.agent.select_action(
+                        state, action, reward, hidden_p, EPS_up=False, evaluate=True
+                    )
+                    next_state, reward, terminated, truncated, _ = self.env.step(
+                        action)
+                    total_reward += reward
+                    state = next_state["image"].astype(np.float32).reshape(-1)
+                    if terminated or truncated:
+                        break
+            avg_reward = total_reward / episode_range
+            if avg_reward != 0:
+                avg_reward = 1/avg_reward
+            return avg_reward
 
-            state = next_state["image"].astype(np.float32).reshape(-1)
-            if terminated or truncated:
-                break
-        logging.debug(
-            f"State: {self.state}, BestState: {self.best_state}, Reward: {total_reward}")
-        avg_reward = total_reward
-        return avg_reward
+        else:
+            for _ in range(episode_range):
+                agent_copy = self.agent
+                memory_copy = self.memory
+                numsteps = 0
+                while numsteps <= total_numsteps:
+                    hidden_p = agent_copy.get_initial_hidden()
+                    action = -1  # placeholder
+                    reward = 0
+                    state = self.env.reset()[0]["image"].astype(
+                        np.float32).reshape(-1)
+
+                    ep_hiddens = [hidden_p]  # z[-1]
+                    ep_actions = [action]  # a[-1]
+                    ep_rewards = [reward]  # r[-1]
+                    ep_states = [state]  # o[0]
+
+                    while True:
+                        # never used
+                        action, hidden_p = agent_copy.select_action(
+                            state,
+                            action,
+                            reward,
+                            hidden_p,
+                            EPS_up=True,
+                            evaluate=False,
+                        )
+                        next_state, reward, terminated, truncated, _ = self.env.step(
+                            action)  # Step
+
+                        state = next_state["image"].astype(
+                            np.float32).reshape(-1)
+                        ep_hiddens.append(hidden_p)  # z[t]
+                        ep_actions.append(action)  # a[t]
+                        ep_rewards.append(reward)  # r[t]
+                        ep_states.append(state)  # o[t+1]
+                        total_reward += reward
+                        if (
+                            len(memory_copy) > self.args["batch_size"]
+                            and numsteps % self.args["rl_update_every_n_steps"] == 0
+                        ):
+                            losses = agent_copy.update_parameters(
+                                memory_copy, self.args["batch_size"], self.args["rl_updates_per_step"]
+                            )
+                        numsteps += 1
+                        if terminated or truncated:
+                            break
+                memory_copy.push(
+                    ep_states, ep_actions, ep_rewards, ep_hiddens)
+            avg_reward = total_reward / episode_range
+            if avg_reward != 0:
+                avg_reward = 1/avg_reward
+            return avg_reward
 
 
 def run_exp(args):
@@ -135,6 +188,7 @@ def run_exp(args):
                     running_losses if total_numsteps > 0 else None,
                     FPS if total_numsteps > 0 else None,
                 )
+
                 # running metrics
                 k_episode = 0  # num of env episodes
                 k_updates = 0  # num of agent updates
@@ -189,7 +243,7 @@ def run_exp(args):
                     }
 
             if isinstance(env, DynamicCrossingEnv):
-                if total_numsteps == 1000000 and total_numsteps > 0:
+                if total_numsteps == args["env_change"] and total_numsteps > 0:
                     print(f"Increasing_size at {total_numsteps} steps")
                     if args["env_name"] == "DynamicCrossingEnv":
                         env = DynamicCrossingEnv(
@@ -214,18 +268,7 @@ def run_exp(args):
                     env.reset(seed=seed)
                     test_env.reset(seed=seed+1)
 
-            if total_numsteps == 1000000:
-                if args['zero_shot'] == True:
-                    logger.configure(dir=args['logdir'], format_strs=[
-                        'csv'], log_suffix="progress_before_lahc")
-                    log_and_test(
-                        test_env,
-                        agent,
-                        total_numsteps,
-                        running_metrics if total_numsteps > 0 else None,
-                        running_losses if total_numsteps > 0 else None,
-                        FPS if total_numsteps > 0 else None,
-                    )
+            if total_numsteps == args["lahc"]:
                 activation_functions = [
                     'softplus-softplus',
                     'gelu-gelu',
@@ -235,12 +278,11 @@ def run_exp(args):
                 ]
 
                 initial_state = np.random.randint(len(activation_functions))
-                lahc.LateAcceptanceHillClimber.steps_minimum = 100000
-                lahc.LateAcceptanceHillClimber.history_length = 5000
-                lahc.LateAcceptanceHillClimber.updates_every = 100
-                lahc.LateAcceptanceHillClimber.steps_idle_fraction = 0.01
+                lahc.LateAcceptanceHillClimber.steps_minimum = 20
+                lahc.LateAcceptanceHillClimber.history_length = 3
+                lahc.LateAcceptanceHillClimber.updates_every = 1
                 optimizer = ActivationFunctionOptimizer(
-                    initial_state, agent, env, activation_functions)
+                    initial_state, agent, env, activation_functions, memory, args)
                 optimizer.run()
                 optimized = optimizer.best_state
                 best_activation_function = activation_functions[optimized]
@@ -250,17 +292,6 @@ def run_exp(args):
                 agent.critic_target.update_activation_function(
                     best_activation_function)
                 args['af'] = best_activation_function
-                if args['zero_shot'] == True:
-                    logger.configure(dir=args['logdir'], format_strs=[
-                        'csv'], log_suffix="progress_after_lahc")
-                    log_and_test(
-                        test_env,
-                        agent,
-                        total_numsteps,
-                        running_metrics if total_numsteps > 0 else None,
-                        running_losses if total_numsteps > 0 else None,
-                        FPS if total_numsteps > 0 else None,
-                    )
 
             total_numsteps += 1
 
@@ -286,16 +317,14 @@ def log_and_test(
     running_metrics,
     running_losses,
     FPS,
-    evaluate_only=False
 ):
-    if not evaluate_only:
-        logger.record_step("env_steps", total_numsteps)
-        if total_numsteps > 0:
-            for k, v in running_metrics.items():
-                logger.record_tabular("train/" + k, v)
-            for k, v in running_losses.items():
-                logger.record_tabular(k, v)
-            logger.record_tabular("FPS", FPS)
+    logger.record_step("env_steps", total_numsteps)
+    if total_numsteps > 0:
+        for k, v in running_metrics.items():
+            logger.record_tabular("train/" + k, v)
+        for k, v in running_losses.items():
+            logger.record_tabular(k, v)
+        logger.record_tabular("FPS", FPS)
 
     metrics = {
         k: 0.0
